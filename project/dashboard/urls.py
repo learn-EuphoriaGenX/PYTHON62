dfefb8ecf49356c581aaea468732fd22fa395411
Rounak <rounakbasak.personal@gmail.com>
2026-03-31 12:03:59 +0530

    add database

--- project/dashboard/urls.py
+++ project/dashboard/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('overview/', views.Overview, name='overview'),
+    path('add-car/', views.Add_car, name='add-car'),
 ]
--- project/dashboard/views.py
+++ project/dashboard/views.py
@@ -1,11 +1,58 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Car
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 from .models import Car
 
 # Create your views here.
+@login_required(login_url='login')
 def Overview(request):
+    id = request.GET.get('id')
+    car = None
+    if id:
+        car = Car.objects.filter(id=id, user=request.user).first()
+    else:
+        car = Car.objects.filter(user=request.user).first()
+    
 
-    my_cars = Car.objects.filter(user = request.user)
-    return render(request, 'overview.html', {'data':my_cars})
+    if not car:
+        messages.info(request, "No cars found. Please add a car to view the overview.")
+        return redirect('add-car')
+    return render(request, 'overview.html', {'car': car})
+
+@login_required(login_url='login')
+def Add_car(request):
+    if request.method == 'POST':
+        car_name = request.POST.get('car_name')
+        car_image = request.FILES.get('car_image')
+        max_speed = request.POST.get('max_speed')
+        car_engine = request.POST.get('car_engine')
+        charging_port = request.POST.get('charging_port')
+        maintenance_cycle = request.POST.get('maintenance_cycle')
+        seat_capacity = request.POST.get('seat_capacity')
+        audio_system = request.POST.get('audio_system')
+
+        if car_name and car_image and max_speed and car_engine and charging_port and maintenance_cycle and seat_capacity and audio_system:
+            new_car = Car(
+                user=request.user,
+                name=car_name,
+                image=car_image,
+                max_speed=max_speed,
+                engine=car_engine,
+                charging_port=charging_port,
+                maintenance_cycle=maintenance_cycle,
+                seat_capacity=seat_capacity,
+                audio_system=audio_system
+            )
+            new_car.save()
+            messages.success(request, "Car added successfully!")
+            return redirect('overview')
+        else:
+            print("Please fill in all fields.")
+            messages.error(request, "Please fill in all fields.")
+            return redirect('add-car')
+       
+    return render(request, 'add-car.html')
 
 def SlotBooking(request):
     pass
